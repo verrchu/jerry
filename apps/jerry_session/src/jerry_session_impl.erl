35c69d05bf55b66875555201f26ff72a97c0f949
Yauheni Tsiarokhin <yauheni@tsiarokhin.me>
2020-12-01 23:57:48 +0300

    some work on session state mechine

--- apps/jerry_session/src/jerry_session_impl.erl
+++ apps/jerry_session/src/jerry_session_impl.erl
@@ -2,6 +2,17 @@
 
 -behaviour(gen_statem).
 
+-include_lib("kernel/include/logger.hrl").
+
+-record(data, {reminder_description}).
+
+-define(STATE_AWAITING_COMMAND(), awaiting_command).
+-define(STATE_AWAITING_REMINDER_DESCRIPTION(), awaiting_reminder_description).
+-define(STATE_AWAITING_REMINDER_INTERVAL(), awaiting_reminder_interval).
+
+-define(COMMAND_NEW(), <<"/new">>).
+-define(COMMAND_CANCEL(), <<"/cancel">>).
+
 -export([
     init/1,
     terminate/3,
@@ -13,9 +24,42 @@
 callback_mode() -> handle_event_function.
 
 init([_ChatId]) ->
-    {ok, {}, {}}.
+    {ok, ?STATE_AWAITING_COMMAND(), #data{}}.
 
-handle_event(_EventType, _EventContent, State, Data) ->
+handle_event({call, From}, ?COMMAND_NEW() = Msg, State0, Data0) ->
+    ?LOG_DEBUG({"msg: ~p; s: ~p; d: ~p", [Msg, State0, Data0]}),
+    ok = gen_statem:reply(From, ok),
+    Data1 = #data{},
+    State1 = ?STATE_AWAITING_REMINDER_DESCRIPTION(),
+    ?LOG_DEBUG({"os: ~p; ns: ~p; od: ~p; nd: ~p", [State0, State1, Data0, Data1]}),
+    {next_state, State1, Data1};
+handle_event({call, From}, ?COMMAND_CANCEL() = Msg, State0, Data0) ->
+    ?LOG_DEBUG({"msg: ~p; s: ~p; d: ~p", [Msg, State0, Data0]}),
+    ok = gen_statem:reply(From, ok),
+    Data1 = #data{},
+    State1 = ?STATE_AWAITING_COMMAND(),
+    ?LOG_DEBUG({"os: ~p; ns: ~p; od: ~p; nd: ~p", [State0, State1, Data0, Data1]}),
+    {next_state, State1, Data1};
+handle_event({call, From}, Msg, ?STATE_AWAITING_REMINDER_DESCRIPTION() = State0, Data0) ->
+    ?LOG_DEBUG({"msg: ~p; s: ~p; d: ~p", [Msg, State0, Data0]}),
+    ok = gen_statem:reply(From, ok),
+    Data1 = Data0#data{reminder_description = Msg},
+    State1 = ?STATE_AWAITING_REMINDER_INTERVAL(),
+    ?LOG_DEBUG({"os: ~p; ns: ~p; od: ~p; nd: ~p", [State0, State1, Data0, Data1]}),
+    {next_state, State1, Data1};
+handle_event({call, From}, Msg, ?STATE_AWAITING_REMINDER_INTERVAL() = State0, Data0) ->
+    ?LOG_DEBUG({"msg: ~p; s: ~p; d: ~p", [Msg, State0, Data0]}),
+    ok = gen_statem:reply(From, ok),
+    Data1 = #data{},
+    State1 = ?STATE_AWAITING_COMMAND(),
+    ?LOG_DEBUG({"os: ~p; ns: ~p; od: ~p; nd: ~p", [State0, State1, Data0, Data1]}),
+    {next_state, State1, Data1};
+handle_event({call, From}, Msg, State, Data) ->
+    ?LOG_DEBUG({"[UNEXPECTED] msg: ~p; s: ~p; d: ~p", [{call, Msg}, State, Data]}),
+    ok = gen_statem:reply(From, ok),
+    {next_state, State, Data};
+handle_event(MsgType, Msg, State, Data) ->
+    ?LOG_DEBUG({"[UNEXPECTED] msg: ~p; s: ~p; d: ~p", [{MsgType, Msg}, State, Data]}),
     {next_state, State, Data}.
 
 terminate(_Reason, _State, _Data) -> ok.
