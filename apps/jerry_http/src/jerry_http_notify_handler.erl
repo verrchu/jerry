e653f6749d37f744b634fcd08fe05d982b9f5e5b
Yauheni Tsiarokhin <yauheni@tsiarokhin.me>
2020-12-02 20:46:15 +0300

    add body parsing

--- apps/jerry_http/src/jerry_http_notify_handler.erl
+++ apps/jerry_http/src/jerry_http_notify_handler.erl
@@ -5,7 +5,9 @@
 -export([init/2]).
 
 init(Req0, Opts) ->
-    {ok, Body, Req1} = cowboy_req:read_body(Req0),
-    ?LOG_DEBUG({"EVENT :: ~p", [{body, Body}]}),
+    {ok, Body0, Req1} = cowboy_req:read_body(Req0),
     Req2 = cowboy_req:reply(200, Req1),
+    Body1 = jiffy:decode(Body0, [return_maps]),
+    ?LOG_DEBUG({"http msg: ~p", [Body1]}),
+    ok = jerry_session:recv(Body1),
     {ok, Req2, Opts}.
--- apps/jerry_session/src/jerry_session.erl
+++ apps/jerry_session/src/jerry_session.erl
@@ -3,6 +3,9 @@
 -define(SERVER, jerry_session_impl).
 
 -export([start_monitor/1]).
+-export([recv/1]).
 
 start_monitor(ChatId) ->
     gen_statem:start_monitor(?SERVER, [ChatId], []).
+
+recv(Msg) -> ok.
